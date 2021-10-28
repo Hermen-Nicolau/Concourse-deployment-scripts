8bfae89df16a1ea933e547672c7a93b49e87458f
Hermen-Nicolau <hermen.nicolau@gmail.com>
2021-10-28 17:41:10 -0400

    added the brew link command to the script to overwrite the docker-compose

--- Concourse-Docker-Compose/deploy.sh
+++ Concourse-Docker-Compose/deploy.sh
@@ -22,6 +22,7 @@ then
     echo "Installing the latest version of Docker Compose"
     brew install docker-compose
     #sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    brew link --overwrite docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
     docker-compose --version
 
